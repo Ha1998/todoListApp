c12168660cfb51a8d381addf6a6b1ceea2d61c09
Hamidreza <hamidlashgari1998@gmail.com>
2021-06-01 20:58:59 +0430

    Write script to add Todo and styling with css

---
+++ app.js
@@ -0,0 +1,43 @@
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList= document.querySelector('.todo-list')
+
+
+
+
+
+todoButton.addEventListener('click', addTodo)
+
+
+
+
+
+
+
+
+
+function addTodo(event) {
+    event.preventDefault()
+    const todoDiv = document.createElement('div')
+    todoDiv.classList.add('todo')
+
+    const newTodo = document.createElement('li')
+    newTodo.innerText = todoInput.value
+    newTodo.classList.add('todo-item')
+    todoDiv.appendChild(newTodo)
+
+    const compeletedButton = document.createElement('button')
+    compeletedButton.innerHTML = '<i class="fas fa-check"></i>'
+    compeletedButton.classList.add("complete-btn")
+    todoDiv.appendChild(compeletedButton)
+
+
+    const trashButton = document.createElement('button')
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+    trashButton.classList.add("trash-btn")
+    todoDiv.appendChild(trashButton)
+
+
+    todoList.appendChild(todoDiv)
+
+}
